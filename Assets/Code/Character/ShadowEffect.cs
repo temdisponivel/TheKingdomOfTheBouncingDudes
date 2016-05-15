6e5aebf278bae3d32d8589698b07892d582cb9ad
vdBeren <vdias.savariego@gmail.com>
2016-05-15 18:55:39 -0300

    Arrumado script de Sombra
    Script de sombra agora funciona corretamente. Entretanto falta
    implementar uma boa forma de usar o prefab de Sombra.

--- Assets/Code/Character/ShadowEffect.cs
+++ Assets/Code/Character/ShadowEffect.cs
@@ -3,17 +3,23 @@ using System.Collections;
 
 public class ShadowEffect : MonoBehaviour {
 
-	protected GameObject _character;
-	protected Quaternion _trueRotation; 
+	public float _offsetY;
+	protected Quaternion _trueRotation;
+
 
 	// Use this for initialization
 	void Start () {
-		_trueRotation = this.transform.rotation;
+		_trueRotation = new Quaternion(0f, 0f, 0f, 1.0f);
 	}
 		
 	// Update is called once per frame
 	void LateUpdate () {
-		
 		this.transform.rotation = _trueRotation;
+
+		Vector3 p = this.transform.parent.position;
+		Vector3 truePosition = new Vector3 (p.x, p.y + _offsetY, p.z);
+		this.transform.position = truePosition;
+
+
 	}
 }
