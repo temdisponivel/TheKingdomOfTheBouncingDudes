f2eded41f03d50f4f06868f7b18ece446b112165
Matheus <temdisponivel@gmail.com>
2016-06-16 04:46:52 -0300

    Change all textures resolutions and atlas

--- Assets/_Code/Game/DayTimeChanger.cs
+++ Assets/_Code/Game/DayTimeChanger.cs
@@ -8,6 +8,12 @@ namespace BounceDudes
         public GameObject MorningPosition;
         public GameObject AfternonPosition;
         public GameObject NightPosition;
+
+        public SpriteRenderer RenderBg;
+
+        public Sprite MorningBg;
+        public Sprite AfternonBg;
+        public Sprite NightBg;
         
         public void Start()
         {
@@ -16,18 +22,22 @@ namespace BounceDudes
                 case DayTimeSequence.Morning:
                     this.transform.position = MorningPosition.transform.position;
                     GameManager.Instance.CurrentDayTimeSequence = DayTimeSequence.MorningMorning;
+                    RenderBg.sprite = MorningBg;
                     break;
                 case DayTimeSequence.MorningMorning:
                     this.transform.position = MorningPosition.transform.position;
                     GameManager.Instance.CurrentDayTimeSequence = DayTimeSequence.Afternon;
+                    RenderBg.sprite = MorningBg;
                     break;
                 case DayTimeSequence.Afternon:
                     this.transform.position = AfternonPosition.transform.position;
                     GameManager.Instance.CurrentDayTimeSequence = DayTimeSequence.Night;
+                    RenderBg.sprite = AfternonBg;
                     break;
                 case DayTimeSequence.Night:
                     this.transform.position = NightPosition.transform.position;
                     GameManager.Instance.CurrentDayTimeSequence = DayTimeSequence.Morning;
+                    RenderBg.sprite = NightBg;
                     break;
             }
         }
